f65a0d1750e1a7e7369abafe00d3c2ee7909d877
Lukaz019 <ls.lucasbatista@gmail.com>
2024-11-29 14:19:47 -0300

    setup

---
+++ codigo/wwwroot/js/site.js
@@ -0,0 +1,33 @@
+﻿var didScroll
+var lastScrollTop = 0
+const navbarHeight = $('header').outerHeight()
+
+$(window).on("scroll", function () {
+    didScroll = true
+})
+
+$(document).on('ready', function () {
+    hasScrolled()
+})
+
+setInterval(function () {
+    if (didScroll) {
+        hasScrolled()
+        didScroll = false
+    }
+}, 250)
+
+function hasScrolled() {
+    let st = $(this).scrollTop()
+
+    if (Math.abs(lastScrollTop - st) <= 5)
+        return
+
+    if (st > lastScrollTop && st > navbarHeight)
+        $('.main-header').removeClass('nav-up').addClass('nav-down')
+    else
+        if (st < navbarHeight)
+            $('.main-header').removeClass('nav-down').addClass('nav-up')
+
+    lastScrollTop = st
+}
